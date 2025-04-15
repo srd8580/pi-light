e93fd38698a4467ff862dd19a52db25e5fceeb60
Steve Danner <steven.danner@gmail.com>
2025-04-14 20:15:53 -0500

    added vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
-import './style.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import '@fortawesome/fontawesome-free/css/all.css' 
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+
+// Components
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+})
+
+createApp(App).use(vuetify).mount('#app')
